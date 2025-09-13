6df7b7486de69ccd73599ce6396aeff9dc56c2de
ouielba90 <oui.elba.1990@gmail.com>
2025-09-13 13:18:25 +0200

    feat: add updateItem function to update products and added some extra fixes

--- ShoppingList.js
+++ ShoppingList.js
@@ -32,8 +32,33 @@ console.log(lista_compra)
 // Funcion para eliminar un elemento de la lista usando un indice como argumento
 
 function removeItem(index, lista_compra){
-  return lista_compra.splice(2, 1)
+  return lista_compra.splice(5, 1)
 }
 
 removeItem(1,lista_compra)
 
+console.log(12)
+
+// Funcion para actualiza de la lista usando como argumento el indice, el item nuevo y la cantidad
+function updateItem(index, newItem, newQuantity, comprado, lista_compra){
+  let lista_items = []
+  for (let v of lista_compra){
+    lista_items.push(v["item"])
+  }
+  
+  let elemento_actual = lista_compra.find(el => el["item"] === newItem);
+  if (elemento_actual !== undefined){
+    let elemento_actual_index = lista_compra.findIndex(el => el["item"] === newItem);
+      if(lista_compra[elemento_actual_index]["cantidad"] !== newQuantity){
+        lista_compra[elemento_actual_index]["cantidad"] = newQuantity
+        lista_compra[elemento_actual_index]["comprado"] = comprado
+    }
+  } else {
+      lista_compra.splice(index, 0, new entrada(newItem, newQuantity, comprado))
+  }
+} 
+
+
+updateItem(1,"galletas",10, true, lista_compra)
+updateItem(3,"doritos", 8, true, lista_compra)
+console.log(lista_compra)
