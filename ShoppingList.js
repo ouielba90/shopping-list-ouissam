9fd577b106f4d5fc88e9461f0fe9c9f6dcafd0b3
ouielba90 <oui.elba.1990@gmail.com>
2025-09-13 10:43:57 +0200

    feat: add addItem function to insert products

--- ShoppingList.js
+++ ShoppingList.js
@@ -9,3 +9,14 @@ function entrada(item, cantidad, comprado) {
 let nuevo = new entrada("patatas",2,true)
 
 console.log(nuevo)
+
+// Funcion para agregar una entrada a la lista
+
+function addItem(lista_compra, para_agregar){
+  lista_compra.push(para_agregar)
+}
+
+addItem(lista_compra, new entrada("bolsa de patatas lays", 2, false))
+addItem(lista_compra, new entrada("bolsa de cacauetes", 1, false))
+
+console.log(lista_compra)
